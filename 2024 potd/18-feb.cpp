1addc367f4adfbfddef1af6034e2814d13a16dbc
Hridaynath Patil <100073906+hridaypatil@users.noreply.github.com>
2024-02-19 21:28:58 +0530

    Create 18-feb.cpp

---
+++ 2024 potd/18-feb.cpp
@@ -0,0 +1,16 @@
+
+class Solution
+{
+    public:
+        int sumOfLeafNodes(Node *root ){
+            if(!root)
+                return 0;
+            
+            if(!root->left && !root->right)
+                return root->data;
+            
+            return sumOfLeafNodes(root->left) + sumOfLeafNodes(root->right);
+        }
+};
+
+
