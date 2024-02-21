617198e1e97304e063762ff9408cb1bf659d2a58
Hridaynath Patil <100073906+hridaypatil@users.noreply.github.com>
2024-02-21 23:48:53 +0530

    Create 20-feb.cpp

---
+++ 2024 potd/20-feb.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+bool solve(string A, unordered_map<string,int>&mp, int ind){
+        if(ind>=A.length()){
+            return true;
+        }
+        
+        for(int i=ind;i<A.length();i++){
+            string s=A.substr(ind, i-ind+1);
+            if(mp.find(s)!=mp.end()){
+                if(solve(A, mp, i+1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    
+    int func(string A, vector<string> &B) {
+        //code here
+        unordered_map<string,int>mp;
+        for(int i=0;i<B.size();i++){
+            mp[B[i]]++;
+        }
+        
+        if(solve(A, mp, 0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        return func(s, dictionary);
+    }
+};
