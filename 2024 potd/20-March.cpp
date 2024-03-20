dfb4578cf11fe71fa0be6467a147c4768a347ddb
Hridaynath Patil <100073906+hridaypatil@users.noreply.github.com>
2024-03-20 20:49:14 +0530

    Create 20-March.cpp

---
+++ 2024 potd/20-March.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    function<pair<int,int>(Node *)>dfs=[&](Node* node)->pair<int,int>
+    {
+        if(!node)
+            return {0, 0};
+        pair<int,int>left=dfs(node->left);
+        pair<int,int>right=dfs(node->right);
+        pair<int,int>ans;
+        if(left.second>right.second)
+            ans=left;
+        else if(right.second>left.second)
+            ans=right;
+        else
+        {
+            ans.first=max(left.first, right.first);
+            ans.second=left.second;
+        }
+        ans.first+=(node->data);
+        ans.second++;
+        return ans;
+    };
+    
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        return dfs(root).first;
+    }
+};
